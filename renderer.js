44206227387d28009f46a7b7c8b56b71a1168c6f
denys <denamyte@gmail.com>
2024-01-24 18:14:26 +0200

    using preload scripts

--- renderer.js
+++ renderer.js
@@ -2,3 +2,11 @@ const information = document.getElementById('info')
 
 information.innerText = `This app is using Chrome (v${versions.chrome()}),` +
   ` Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const func = async () => {
+  const response = await versions.ping()
+  const p = document.getElementById('main_answer')
+  p.innerText = response
+}
+
+func()
